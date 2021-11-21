1b67f718b7aedb94748b72e35a5cced3e265e35f
chionsuk <hsuk0409@gmail.com>
2021-11-21 23:48:52 +0900

    유저 데이터 유효성 검사 완료, 알림 함수 추가
    알림함수 로그로 변경 필요, 일단은 콘솔 출력

--- web/user/user_handler.py
+++ web/user/user_handler.py
@@ -15,19 +15,28 @@ class UserHandler:
         # 시간 체크
         request_time = args.get("time")
         if not request_time:
+            notice_error_field("time")
             return False
         divide_times = str(request_time).split(":")
         if len(divide_times[0]) > 2 or len(divide_times[1]) > 2:
+            notice_error_field("time")
             return False
 
         # 요일 체크
         day_of_weeks = args.get("dayOfWeek")
         if len(day_of_weeks) < 1:
+            notice_error_field("dayOfWeek")
             return False
 
         # 카테고리 체크
         category = args.get("category")
         if not category:
+            notice_error_field("category")
             return False
 
         return True
+
+
+def notice_error_field(filed_name: str) -> None:
+    # TODO 로그로 변경
+    print(f"{filed_name}값이 비어있거나 잘못 되어 있습니다.")
