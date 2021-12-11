aa0810aad5a3e20fad784fba82f8aac869adfdfd
chionsuk <hsuk0409@gmail.com>
2021-12-11 15:15:41 +0900

    FCM 핸들러 추가

---
+++ web/fcm/fcm_handler.py
@@ -0,0 +1,21 @@
+from pyfcm import FCMNotification
+
+from web.db import settings
+
+
+class FcmHandler:
+
+    def __init__(self, token: str) -> None:
+        self.fcm_service = FCMNotification(api_key=settings.FCM_API_KEY)
+        self.token = token
+
+    def send_message(self, title: str, message_body: dict):
+        data_message = {
+            "title": title,
+            "body": message_body
+        }
+
+        result = self.fcm_service.single_device_data_message(registration_id=self.token,
+                                                             data_message=data_message)
+
+        return result
