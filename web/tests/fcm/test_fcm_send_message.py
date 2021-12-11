9e786c85bdb5e553eaa7631a3e3e0d950a6811e7
chionsuk <hsuk0409@gmail.com>
2021-12-11 17:19:29 +0900

    FCM 푸시 알림 테스트

---
+++ web/tests/fcm/test_fcm_send_message.py
@@ -0,0 +1,13 @@
+from web.db import settings
+from web.fcm.fcm_handler import FcmHandler
+
+
+def test_send_message():
+    fcm_service = FcmHandler(token=settings.TEST_FCM_TOKEN)
+    message_body = {
+        "message": "TEST"
+    }
+    result = fcm_service.send_message("시계는 와치 테스트 메시지", message_body=message_body)
+    print(result)
+
+    assert result is not None
